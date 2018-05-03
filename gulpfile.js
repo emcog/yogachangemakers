87355f41be630595af34ccfdbbc2cfae7fa225a0
emCog <robin.leonard@gmail.com>
2018-05-03 16:03:29 +0100

    migrating from skeleton to bulma.io with respective changes to gulp and index. Bulma has been copied from node_modules to home directory as a temp fix – will be updated once project has appropriate dev and dist structure

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,19 @@ gulp.task('serve', ['sass'], function() {
         server: "./"
     });
 
-    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("bulma/*.sass", ['sass']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("scss/*.scss")
+    return gulp.src("bulma/sass/*.sass")
         .pipe(sass())
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("bulma/css"))
         .pipe(browserSync.stream());
 	});
 
-//
+// –––– image helper needs to be configured/tested
 gulp.task('compass-imagehelper', function () {
     return gulp.src('sources/images/**/*.+(jpeg|jpg|png|gif|svg)')
         .pipe(compassImagehelper({
